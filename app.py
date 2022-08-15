9a27a4738c2d7fc868059444a0fe8887c6497333
Tarek Khaldi <tarekkhaldi1311@gmail.com>
2022-08-15 17:48:55 +0200

    update app.py and requirement.txt

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import dash
 import dash_bootstrap_components as dbc
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
+from dash import html
+from dash.dependencies import Input
+from dash.dependencies import Output
+from dash.dependencies import State
 import requests
 import base64
 from io import BytesIO
-import dash_core_components as dcc
+from dash import dcc
 import plotly.graph_objs as go
 from collections import Counter
 
