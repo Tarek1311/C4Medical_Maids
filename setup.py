b592a7d033370aa083b9cc44af8704728c077060
Tarek Khaldi <tarekkhaldi1311@gmail.com>
2022-08-31 17:42:46 +0200

    Create setup.py file

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 #!/usr/bin/venv python
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import find_packages, setup
 from app import app
 
 setup(
     # Name of the package
-    name=app,
+    name='C4Medical_Maids',
     version='1.0.0',
-    license='',
     description='Notre projet est une application de vente et location de matériels médical et paramédical',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author='Tarek Khaldi',
     author_email='tarekkhaldi1311@gmail.com',
     url='https://github.com/Tarek1311/C4Medical_Maids.git/',
     packages=find_packages(include=['app', 'app.*']),
-    keywords=[],
+    include_package_data=True,
     python_requires='>=3.7, <4',
     install_requires=["pytest",
                       "dash==2.6.1",
