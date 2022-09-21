3b9fcf61aa7cac822aaa90ae952c274fdc19925c
Tarek1311 <96184436+Tarek1311@users.noreply.github.com>
2022-09-21 16:15:41 +0200

    Feature/setup (#10)
    * Fix  setup.py file
    
    * Remove shebang before merging setup.py file
    
    * Editing files app.py, setup.py and connect the packages with the requirements.txt without having duplicate
    
    * Put C4Medical_Maids.egg-info in .gitignore and update readme, app.py, requirements.txt and setup.py
    
    * Update readme file
    
    * Update readme file
    
    * git cache cleared
    
    * Update README.md, add precommit in requirements.txt and add find_packages in setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,17 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 setup(
-    name='C4Medical_Maids',
-    version='1.0.0',
-    description='Notre projet est une application de vente et location de matériels médical et paramédical',
+    name="C4Medical_Maids",
+    version="1.0.0",
+    description="Notre projet est une application de vente et location de matériels médical et paramédical",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author='Tarek Khaldi',
-    author_email='tarekkhaldi1311@gmail.com',
-    url='https://github.com/Tarek1311/C4Medical_Maids.git/',
-    packages=find_packages(include=['app', 'app.*']),
+    author="Tarek Khaldi",
+    author_email="tarekkhaldi1311@gmail.com",
+    url="https://github.com/Tarek1311/C4Medical_Maids.git/",
     include_package_data=True,
-    python_requires='>=3.7, <4',
-    install_requires=["pytest",
-                      "dash==2.6.1",
-                      "dash-bootstrap-components==1.2.1",
-                      "requests==2.28.1",
-                      "gunicorn==20.1.0",
-                      "dash-auth==1.3.2"
-                      ],
+    python_requires=">=3.7, <4",
+    install_requires=open("requirements.txt").read().splitlines(),
     entry_points={"console_scripts": ["run = app.app:run"]},
+    packages=find_packages(exclude=["tests"]),
 )
